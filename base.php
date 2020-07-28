f4cd3036412902b6ae002d428b7ef0e34d75e9d6
unknown <a831202a@gmail.com>
2020-07-28 23:59:38 +0800

    登入柳覽

--- base.php
+++ base.php
@@ -1,127 +1,161 @@
- <?php
-
-$dsn="mysql:host=localhost;charset=utf8;dbname=resume";
-$pdo=new PDO($dsn,"root","");
+<?php
 date_default_timezone_set("Asia/Taipei");
 session_start();
 
-//查詢全部
-function all($table,...$arg){
-    global $pdo;
-    $sql="select * from $table ";
-    
-    if(isset($arg[0]) && is_array($arg[0])){
-        $tmp=[];
-        foreach($arg[0] as $key => $value){
-            $tmp[]=sprintf("`%s`='%s'",$key,$value);
+class DB
+{
+
+    private $dsn = "mysql:host=localhost;charset=utf8;dbname=db02";
+    private $root = "root";
+    private $password = "";
+    private $table;
+    private $pdo;
+
+    //設定建構式
+    public function __construct($table)
+    {
+
+        $this->table = $table;
+
+        $this->pdo = new PDO($this->dsn, $this->root, $this->password);
+    }
+
+    public function all(...$arg)
+    {
+
+        $sql = "select * from $this->table ";
+
+        if (!empty($arg[0]) && is_array($arg[0])) {
+            foreach ($arg[0] as $key => $value) {
+                $tmp[] = sprintf("`%s`='%s'", $key, $value);
+            }
+
+            $sql = $sql . " where " . implode(" && ", $tmp);
         }
-        $sql=$sql." where " . implode(" && ",$tmp);
-    }
 
-    if(isset($arg[1])){
-        $sql=$sql . $arg[1];
-    }
-
-    //echo $sql;
-
-    return $pdo->query($sql)->fetchAll();
-}
-
-//刪除資料
-function del($table,$arg){
-    global $pdo;
-    $sql="delete from $table ";
-    
-    if(is_array($arg)){
-        $tmp=[];
-        foreach($arg as $key => $value){
-            $tmp[]=sprintf("`%s`='%s'",$key,$value);
+        if (!empty($arg[1])) {
+            $sql = $sql . $arg[1];
         }
-        $sql=$sql." where " . implode(" && ",$tmp);
-    }else{
-        $sql=$sql." where `id`='$arg'";
+
+        //echo $sql;
+        return $this->pdo->query($sql)->fetchAll();
     }
 
-   // echo $sql;
-    return $pdo->exec($sql);
-}
+    public function find($arg)
+    {
 
-//查詢單筆
-function find($table,$arg){
-    global $pdo;
+        $sql = "select * from $this->table ";
 
-    $sql="select * from $table ";
+        if (is_array($arg)) {
 
-    if(is_array($arg)){
-        $tmp=[];
-        foreach($arg as $key => $value){
-            $tmp[]=sprintf("`%s`='%s'",$key,$value);
+            foreach ($arg as $key => $value) {
+                $tmp[] = sprintf("`%s`='%s'", $key, $value);
+            }
+
+            $sql = $sql . " where " . implode(" && ", $tmp);
+
+        } else {
+            $sql = $sql . " where `id`='$arg'";
         }
-        $sql=$sql." where " . implode(" && ",$tmp);
-    }else{
-        $sql=$sql." where `id`='$arg'";
+
+        //echo $sql;
+        return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
     }
-
-    //echo $sql;
-
-    return $pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
-}
 
 //計算筆數
-function nums($table,...$arg){
-    global $pdo;
-    $sql="select count(*) from $table ";
-    
-    if(isset($arg[0]) && is_array($arg[0])){
-        $tmp=[];
-        foreach($arg[0] as $key => $value){
-            $tmp[]=sprintf("`%s`='%s'",$key,$value);
-        }
-        $sql=$sql." where " . implode(" && ",$tmp);
-    }
+    public function count(...$arg)
+    {
 
-    if(isset($arg[1])){
-        $sql=$sql . $arg[1];
-    }
+        $sql = "select count(*) from $this->table ";
 
-    //echo $sql;
+        if (!empty($arg[0]) && is_array($arg[0])) {
 
-    return $pdo->query($sql)->fetchColumn();
-}
-
-//萬用查詢
-function q($sql){
-    global $pdo;
-
-    return $pdo->query($sql)->fetchAll();
-
-}
-
-
-//新增或更新資料
-function save($table,$arg){
-    global $pdo;
-    if(isset($arg['id'])){
-        //update
-        foreach($arg as $key => $value){
-            if($key!='id'){
-                $tmp[]=sprintf("`%s`='%s'",$key,$value);
+            foreach ($arg[0] as $key => $value) {
+                $tmp[] = sprintf("`%s`='%s'", $key, $value);
             }
-        }
-    
-        $sql="update $table set ".implode(',',$tmp)." where `id`='".$arg['id']."'";
-    }else{
-        //insert
 
-        $sql="insert into $table (`".implode("`,`",array_keys($arg))."`) values('".implode("','",$arg)."')";
+            $sql = $sql . " where " . implode(" && ", $tmp);
+        }
+
+        if (!empty($arg[1])) {
+            $sql = $sql . $arg[1];
+        }
+
+        return $this->pdo->query($sql)->fetchColumn();
+
     }
 
-    //echo $sql;
-    return $pdo->exec($sql);
+    public function save($arg)
+    {
+
+        if (!empty($arg['id'])) {
+            //更新
+            foreach ($arg as $key => $value) {
+                if ($key != 'id') {
+                    $tmp[] = sprintf("`%s`='%s'", $key, $value);
+                }
+            }
+
+            $sql = "update $this->table set " . implode(",", $tmp) . " where `id`='" . $arg['id'] . "'";
+
+        } else {
+            //新增
+            $sql = "insert into $this->table (`" . implode("`,`", array_keys($arg)) . "`) values('" . implode("','", $arg) . "')";
+
+        }
+
+        return $this->pdo->exec($sql);
+    }
+
+    public function del($arg)
+    {
+
+        $sql = "delete from $this->table ";
+
+        if (is_array($arg)) {
+
+            foreach ($arg as $key => $value) {
+                $tmp[] = sprintf("`%s`='%s'", $key, $value);
+            }
+
+            $sql = $sql . " where " . implode(" && ", $tmp);
+
+        } else {
+            $sql = $sql . " where `id`='$arg'";
+        }
+
+        //echo $sql;
+        return $this->pdo->exec($sql);
+    }
+
+    public function q($sql)
+    {
+        return $this->pdo->query($sql)->fetchAll();
+    }
+
 }
 
-//頁面導向
-function to ($url){
-    header("location:".$url);
+function to($url)
+{
+    header("location:" . $url);
 }
 
+$Visited=new DB('visited');
+$News=new DB('news');
+$Que=new DB('que');
+$User=new DB('user');
+$Log=new DB('log');
+
+$visited=$Visited->find(['date'=>date("Y-m-d")]);
+if(empty($visited)){
+    $Visited->save(["date"=>date("Y-m-d"),"total"=>1]);
+    $visited=$Visited->find(['date'=>date("Y-m-d")]);
+    // $_SESSION["visited"]=1;
+}
+
+if(empty($_SESSION['visited'])){
+$_SESSION['visited']=$visited['total'];
+
+}
+
+?>
--- index.php
+++ index.php
@@ -1,4 +1,7 @@
-﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+﻿<?php 
+include_once "base.php";
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0039) -->
 <html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
@@ -15,9 +18,14 @@
 <iframe name="back" style="display:none;"></iframe>
 	<div id="all">
     	<div id="title">
-        00 月 00 號 Tuesday | 今日瀏覽: 1 | 累積瀏覽: 36        </div>
+        <?=date("m 月 d 日 l"); ?> | 今日瀏覽: <?= $_SESSION['visited'];?>| 累積瀏覽:  <?php $Visited->q("select sum(`total`) form `visited`")[0][0];?> 
+		
+		<a href="index.php" style="float: right;">回首頁</a>
+		 </div>
         <div id="title2">
-        	
+		<a href="index.php" title="健康促進往-回首頁">
+        	<img src="icon/02B01.jpg" alt="健康促進往-回首頁">
+			</a>
         </div>
         <div id="mm">
         	<div class="hal" id="lef">
@@ -29,7 +37,12 @@
                	                 </div>
             <div class="hal" id="main">
             	<div>
-            		
+				<span style="width:80%; display:inline-block;">
+                    	    <marquee >
+							請民眾踴躍投稿電子報，讓電子報成為大家相互交流，分享的園地！詳見最新文章。
+							</marquee>
+
+                    </span>
                 	<span style="width:18%; display:inline-block;">
                     	                    	<a href="?do=login">會員登入</a>
                     	                    </span>
@@ -41,7 +54,7 @@
         <div id="bottom">
     	    本網站建議使用：IE9.0以上版本，1024 x 768 pixels 以上觀賞瀏覽 ， Copyright © 2012健康促進網社群平台 All Right Reserved 
     		 <br>
-    		 服務信箱：health@test.labor.gov.tw<img src="./home_files/02B02.jpg" width="45">
+    		 服務信箱：health@test.labor.gov.tw<img src="./icon/02B02.jpg" width="45">
         </div>
     </div>
 
